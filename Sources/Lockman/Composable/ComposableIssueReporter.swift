9db77d5a4885bd622781eda00ade39584703fe0f
Takeshi Shimada <expensivegasprices@gmail.com>
2025-06-28 01:25:50 +0900

    refactor: remove deprecated TCAIssueReporter and configureTCAReporting APIs
    - Remove deprecated TCAIssueReporter typealias
    - Remove deprecated configureTCAReporting() method
    - Keep only the new ComposableIssueReporter and configureComposableReporting() APIs

--- Sources/Lockman/Composable/ComposableIssueReporter.swift
+++ Sources/Lockman/Composable/ComposableIssueReporter.swift
@@ -11,25 +11,10 @@ public enum ComposableIssueReporter: LockmanIssueReporter {
   }
 }
 
-/// Backward compatibility typealias.
-@available(
-  *, deprecated, renamed: "ComposableIssueReporter", message: "Use ComposableIssueReporter instead"
-)
-public typealias TCAIssueReporter = ComposableIssueReporter
-
 /// Configures Lockman to use ComposableArchitecture's issue reporting.
 extension LockmanIssueReporting {
   /// Configures Lockman to use ComposableArchitecture's reportIssue function.
   public static func configureComposableReporting() {
     reporter = ComposableIssueReporter.self
-  }
-
-  /// Backward compatibility method.
-  @available(
-    *, deprecated, renamed: "configureComposableReporting",
-    message: "Use configureComposableReporting() instead"
-  )
-  public static func configureTCAReporting() {
-    configureComposableReporting()
   }
 }
